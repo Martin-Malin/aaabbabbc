f9e5276d4bda1d6509b7a8b3bd1c0946e26b9450
Martin-Malin <130313822+Martin-Malin@users.noreply.github.com>
2023-11-08 10:00:06 +0100

    Premier commit
    Premier commit, fonctionnel

---
+++ aaabbabbc/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static List<string> items = new List<string> { "aa", "ab", "ba", "bb", "c" };
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("?");
+        string input = Console.ReadLine();
+
+        List<string> value = new List<string>();
+        int i = 0;
+        while (value.Count < 4)
+        {
+            if (input[i] == 'c')
+            {
+                value.Add("c");
+                i += 1;
+            }
+            else
+            {
+                value.Add(input.Substring(i, 2));
+                i += 2;
+            }
+        }
+
+        foreach (string item in items)
+        {
+            if (!value.Contains(item))
+            {
+                Console.WriteLine(item);
+                break;
+            }
+        }
+    }
+}
